2c42f1cf6670f7d2a54d4c79719491d0b7199efe
RashadNazer <rashadnazer7@gmail.com>
2021-12-24 11:17:34 +0000

    Update main.py
    Functions withdraw and update_csv implemented

--- Part 1/main.py
+++ Part 1/main.py
@@ -1,11 +1,10 @@
 import csv
-import datetime
-today=datetime.date.today()
-
+from datetime import datetime
+time=datetime.now()
+current_time=time.strftime("%d/%m/%y %H:%M:%S")
 
 customer=open("bank.csv","r")
 bank_csv=csv.reader(customer)
-header= next(bank_csv)
 bank_data=[row for row in bank_csv]
 customer.close()
 session_transaction=[]
@@ -43,19 +42,52 @@ def deposit(acc_number):
     for row in bank_data:
         if acc_number==row[0]:
             row[3]=new_balance
-    transaction(acc_number, "Deposit", amount)
+    transaction(acc_number, "Deposit", amount, new_balance)
+    update_csv(bank_data)
+
+def withdraw(acc_number):
+    '''
+    Function that withdraws amount from account.
+
+    '''
+    print("Enter the amount to be withdrawn: ")
+    amount=input()
+    for row in bank_data:
+        if acc_number==row[0]:
+            current_balance=row[3]
+    new_balance=int(current_balance)-int(amount)
+    if int(cur)<0:
+        
+        for row in bank_data:
+            if acc_number==row[3]:
+                row[3]=new_balance
+    else:
+        print("Insufficient Balance")
+    transaction(acc_number, "Withdraw", amount, new_balance)
+    update_csv(bank_data)
     
-def transaction(acc_number, char, amount):
+    
+def transaction(acc_number, char, amount, new_balance):
     '''
     Function that records transaction details and store it in transaction.csv file
 
     '''
-    time=today.ctime()
-    this_transaction=[acc_number,char,amount,time]
+    this_transaction=[acc_number, char ,amount, new_balance, current_time]
     session_transaction.extend(this_transaction)
     print(session_transaction)
+    new_trans=open("transactions.csv","a", newline="")
+    update_trans=csv.writer(new_trans)
+    update_trans.writerow(session_transaction)
     
-    
+def update_csv(bank_data):
+    '''
+    Function that updates banks.csv file
+
+    '''
+    new_data=open("bank.csv","w+",newline="")
+    update_bank=csv.writer(new_data)
+    for row in bank_data:
+        update_bank.writerow(row)
 
 
 def menu(acc_number):
@@ -63,7 +95,6 @@ def menu(acc_number):
     Function that displays menu and redirects the user to appropriate function based on the user's input
 
     '''
-    
     print("MENU".center(100))
     print("1. Deposit".center(100))
     print("2. Withdraw".center(100))
@@ -75,7 +106,7 @@ def menu(acc_number):
         deposit(acc_number)
         
     elif choice== '2':
-        withdraw()
+        withdraw(acc_number)
     
     elif choice =='3':
         transfer()
