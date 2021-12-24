39827601ed0dc604cd1c3043879ffab2f1436c20
RashadNazer <rashadnazer7@gmail.com>
2021-12-24 19:56:07 +0000

    Update main.py
    Functions start_transfer and change_pin implemented. Program also divided into two classes: customer_account and online_bank

--- Part 1/main.py
+++ Part 1/main.py
@@ -1,197 +1,235 @@
 import csv
+import re
 from datetime import datetime
 time=datetime.now()
 current_time=time.strftime("%d/%m/%y %H:%M:%S")
 
-customer=open("bank.csv","r")
-bank_csv=csv.reader(customer)
-bank_data=[row for row in bank_csv]
-customer.close()
-session_transaction=[]
 
-def login():
-    '''
-    Login function that reads the bank.csv files in the depository and stores the details in a list.
-    It also verifies the credentials the user inputs
-    '''
-    
-    
-    acc_number= input("Enter the Account Number: ")
-    
-    for row in bank_data:
-        if acc_number==row[0]:
-            pin= input("Enter your PIN: ")
-            if pin==row[1]:
-                print("Login Succesful")
-                menu(acc_number)
-                break
-            else:
-                print("Incorrect PIN")
-                break
-    
-def deposit(acc_number):
-    '''
-    Function that deposits money into account
-    '''
-    print("Enter the amount to be deposited: ")
-    amount=input()
-    for row in bank_data:
-        if acc_number==row[0]:
-            current_balance=row[3]
-    new_balance=int(current_balance)+int(amount)
-    for row in bank_data:
-        if acc_number==row[0]:
-            row[3]=new_balance
-    transaction(acc_number, "Deposit", amount, new_balance)
-    update_csv(bank_data)
 
-def withdraw(acc_number):
-    '''
-    Function that withdraws amount from account.
-
-    '''
-    print("Enter the amount to be withdrawn: ")
-    amount=input()
-    for row in bank_data:
-        if acc_number==row[0]:
-            current_balance=row[3]
-            
-    new_balance=int(current_balance)-int(amount)
+class customer_account:
+    customer=open("bank.csv","r")
+    bank_csv=csv.reader(customer)
+    bank_data=[row for row in bank_csv]
+    customer.close()
+    session_transaction=[]
     
-    if int(current_balance) > int(amount):
-        for row in bank_data:
+    
+    def _init_(self,bank_data, session_transaction):
+        self.bank_data=bank_data   
+        self.session_transaction=session_transaction
+    
+    
+    def login(self):
+        '''
+        Login function that reads the bank.csv files in the depository and stores the details in a list.
+        It also verifies the credentials the user inputs
+        '''
+        acc_number= input("Enter the Account Number: ")
+        for row in self.bank_data:
+            if acc_number==row[0]:
+                pin= input("Enter your PIN: ")
+                if pin==row[1]:
+                    print("Login Succesful")
+                    customer_account.menu(self, acc_number)
+                    break
+                else:
+                    print("Incorrect PIN")
+                    break
+    
+    def deposit(self,acc_number):
+        '''
+        Function that deposits money into account
+        '''
+        print("Enter the amount to be deposited: ")
+        amount=input()
+        for row in self.bank_data:
+            if acc_number==row[0]:
+                current_balance=row[3]
+        new_balance=int(current_balance)+int(amount)
+        for row in self.bank_data:
             if acc_number==row[0]:
                 row[3]=new_balance
-                transaction(acc_number, "Withdraw", amount, new_balance)
-                update_csv(bank_data)
-                
-    else:
-        print("Insufficient Balance")
-        
-        
-def transfer(acc_number):
-    '''
-    Function that transfers money between accounts in the same bank
-    '''
-    
-    print("Enter the recipient account number: ")
-    rec_number=input()
-    if acc_number==rec_number:
-        print("You cannot transfer into your own account")
-    p=0
-    for row in bank_data:
-        if rec_number==row[0]:
-            print("Account found")
-            p=1
-            rec_balance=row[3]
-            print("Enter the amount to be transferred: ")
-            amount=input()
-            for row in bank_data:
-                
+        online_bank.transaction(self, acc_number, "Deposit", amount, new_balance)
+        online_bank.update_csv(self)
+
+    def withdraw(self, acc_number):
+        '''
+        Function that withdraws amount from account.
+
+        '''
+        print("Enter the amount to be withdrawn: ")
+        amount=input()
+        for row in self.bank_data:
+            if acc_number==row[0]:
+                current_balance=row[3]
+        new_balance=int(current_balance)-int(amount)
+        if int(current_balance) > int(amount):
+            for row in self.bank_data:
                 if acc_number==row[0]:
-                    
-                    current_balance=row[3]
-                    if int(current_balance)> int(amount):
-                        
-                        sender_balance=int(current_balance)-int(amount)
-                        rec_balance=int(rec_balance)+int(amount)
-                        row[3]=sender_balance
-                        
-                        transaction(acc_number, "Transfer Out", amount, sender_balance)
-                        for row in bank_data:
-                            if rec_number==row[0]:
-                                row[3]=rec_balance
-                                transaction(rec_number, "Transfer IN", amount, rec_balance)
-                                update_csv(bank_data)
-                                return
-               
+                    row[3]=new_balance
+                    online_bank.transaction(self, acc_number, "Withdraw", amount, new_balance)
+                    online_bank.update_csv(self)
+        else:
+            print("Insufficient Balance")
+
+    
+    def start_transfer(self, acc_number):
+        '''
+        Function that initiates transfer function.
+
+    
+        '''
+        print("Enter the recipient account number: ")
+        rec_number=input()
+        if acc_number==rec_number:
+            print("You cannot transfer into your own account")
+        p=0
+        for row in self.bank_data:
+            if rec_number==row[0]:
+                print("Account found")
+                p=1
+                rec_balance=row[3]
+                print("Enter the amount to be transferred: ")
+                amount=input()
+                if(int(amount)<=1000):
+                    online_bank.transfer(self, acc_number, rec_number, rec_balance, amount)
+                else:
+                    print("Transfer to another account restricted to 1000. Please enter a valid amount.")
+                    customer_account.start_transfer(self, acc_number)
+        while(p!=1):
+            print("Account not found")
+            return
+    
+    def change_pin(self, acc_number):
+        '''
+        Function that changes PIN of user.
+        '''
+        print("""New PIN must have:
+              1. Atleast one number
+              2. Atleast one uppercase character
+              3. Atleast one lowercase character
+              4. Atleast one speacial character
+              5. 6-10 characters long
+              Enter the new PIN: """)
+        new_pin=input()
+        for row in self.bank_data:
+            if acc_number==row[0]:
+                if new_pin ==row[1] or new_pin==row[0] or new_pin==row[2] :
+                    print("You cannot enter current PIN or use your account number or name as PIN. ")
+                    customer_account.change_pin(self, acc_number)
+                else:
+                    password_requirements= "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,10}$"
+                    # compiling regex
+                    password = re.compile(password_requirements)
+      
+                    # searching regex                 
+                    status = re.search(password, new_pin)
+                    if status:
+                        for row in self.bank_data:
+                            if acc_number==row[0]:
+                                row[1]=new_pin
+                                online_bank.update_csv(self)
                     else:
-                        print("Insufficient Balance")
-                        return
-    while(p!=1):
-        print("Account not found")
-        return
-                
-                
+                        print("Invalid Password")
+                        customer_account.change_pin(self, acc_number)
+                        
         
-                
-    
-   
+    def menu(self, acc_number):
+        print("MENU".center(100))
+        print("1. Deposit".center(100))
+        print("2. Withdraw".center(100))
+        print("3. Transfer".center(100))
+        print("4. Change PIN".center(100))
+        print("5. Transaction History".center(100))
+        print("6. Logout".center(100))
+        print("Enter your choice= ")
+        choice= input()
+        if choice == '1':
+            customer_account.deposit(self, acc_number)        
+        elif choice== '2':
+            customer_account.withdraw(self, acc_number)
+        elif choice =='3':
+            customer_account.start_transfer(self, acc_number)
+        elif choice =='4':
+            customer_account.change_pin(self, acc_number)
+        elif choice == '5':
+            online_bank.transaction_history(self, acc_number)
+        elif choice == '6':
+            online_bank.logout(self, acc_number)
+        else:
+            print("Invalid option")
+            customer_account.menu(self, acc_number)
         
-     
-                
-    
-def transaction(acc_number, char, amount, new_balance):
-    '''
-    Function that records transaction details and store it in transaction.csv file
-
-    '''
-    this_transaction=[acc_number, char ,amount, new_balance, current_time]
-    session_transaction.extend(this_transaction)
-    print(session_transaction)
-    new_trans=open("transactions.csv","a", newline="")
-    update_trans=csv.writer(new_trans)
-    update_trans.writerow(session_transaction)
-    session_transaction.clear()
-    
-def update_csv(bank_data):
-    '''
-    Function that updates banks.csv file
-
-    '''
-    new_data=open("bank.csv","w+",newline="")
-    update_bank=csv.writer(new_data)
-    for row in bank_data:
-        update_bank.writerow(row)
-
-def logout(acc_number):
-    '''
-    Function that logs out the user from application
-    '''
-    
-    print("Do you want to logout? (Y/N): ")
-    answer=input()
-    if(answer=="Y" or answer=="y"):
-        login()
+class online_bank(customer_account):
+    def transfer(self, acc_number, rec_number, rec_balance, amount):
+        '''
+        Function that transfers money between accounts in the same bank
+        '''
+        for row in self.bank_data:
+            if acc_number==row[0]:
+                current_balance=row[3]
+                if int(current_balance)> int(amount):
+                    sender_balance=int(current_balance)-int(amount)
+                    rec_balance=int(rec_balance)+int(amount)
+                    row[3]=sender_balance
+                    online_bank.transaction(self, acc_number, "Transfer Out", amount, sender_balance)
+                    for row in self.bank_data:
+                        if rec_number==row[0]:
+                            row[3]=rec_balance
+                            online_bank.transaction(self,rec_number, "Transfer IN", amount, rec_balance)
+                            online_bank.update_csv(self)
+                            return
+                else:
+                    print("Insufficient Balance")
+                    return       
         
-    elif(answer=="N" or answer=="n"):
-        menu(acc_number)
+                    
         
-    else:
-        print("Enter a valid response")
-        logout(acc_number)
-    
+    def transaction(self, acc_number, char, amount, new_balance):
+        '''
+        Function that records transaction details and store it in transaction.csv file
 
-def menu(acc_number):
-    '''
-    Function that displays menu and redirects the user to appropriate function based on the user's input
-
-    '''
-    print("MENU".center(100))
-    print("1. Deposit".center(100))
-    print("2. Withdraw".center(100))
-    print("3. Transfer".center(100))
-    print("4. Logout".center(100))
-    print("Enter your choice= ")
-    choice= input()
-    if choice == '1':
-        deposit(acc_number)
+        '''
+        this_transaction=[acc_number, char ,amount, new_balance, current_time]
+        self.session_transaction.extend(this_transaction)
+        print(self.session_transaction)
+        new_trans=open("transactions.csv","a", newline="")
+        update_trans=csv.writer(new_trans)
+        update_trans.writerow(self.session_transaction)
+        self.session_transaction.clear()
         
-    elif choice== '2':
-        withdraw(acc_number)
-    
-    elif choice =='3':
-        transfer(acc_number)
+    def update_csv(self):
+        '''
+        Function that updates banks.csv file
+
+        '''
+        new_data=open("bank.csv","w+",newline="")
+        update_bank=csv.writer(new_data)
+        for row in self.bank_data:
+            update_bank.writerow(row)
+
+    def logout(self, acc_number):
+        '''
+        Function that logs out the user from application
+        '''
+        print("Do you want to logout? (Y/N): ")
+        answer=input()
+        if(answer=="Y" or answer=="y"):
+            customer_account.login(self)
+            
+        elif(answer=="N" or answer=="n"):
+            customer_account.menu(self, acc_number)
+            
+        else:
+            print("Enter a valid response")
+            online_bank.logout(acc_number)
+
+    def transaction_history(self, acc_number):
+        history=[]
         
-    elif choice == '4':
-        login()
-        
-    else:
-        print("Invalid option")
-        menu(acc_number)
 
 
 
 
-
-login()
+new_instance=customer_account()
+new_instance.login()
